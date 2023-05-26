1a0aae11a361884c7ff16ad57e8fa9946702efe7
Eddie <Edward@veronicas-Air.attlocal.net>
2023-05-25 21:36:58 -0700

    function to dsiplay event types

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,34 @@
-var eventbriteUrl = 'https://www.eventbriteapi.com/v3/events/event_id/display_settings/';
+var searchDisplay = document.getElementsByClassName('.form');
+var fetchButton = document.getElementById('format')
+var ticketmasterURL = "https://app.ticketmaster.com/discovery/v2/events.json?&apikey=HLtESgDRQC62k8RSusY2rKZAWIYZkVAw"
 
+function displayEvents(data) {
+  var eventsData = data._embedded.events
+  console.log(data._embedded);
+  for (var i = 0; i < eventsData.length; i++){
+    var eventsearchEl = document.createElement('option');
+    eventsearchEl.textContent = eventsData[i].events;
+   $('.form').append(eventsearchEl);
+  }
+};
+//fetch event search
+
+$.ajax({
+  type:"GET",
+  url: ticketmasterURL,
+  async:true,
+  dataType: "json",
+  success: function(data) {
+              console.log(data);
+            displayEvents(data)
+              // Parse the response.
+              // Do other things.
+           },
+          
+  error: function(xhr, status, err) {
+              // This time, we do not end up here!
+           }
+           
+});
+
+fetchButton.addEventListener('click', displayEvents);
